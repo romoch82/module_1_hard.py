db751a78a254c758659d2f5e582bff87eb598e84
romoch82 <romoch37@gmail.com>
2024-08-06 21:23:57 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,7 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+average = sum(grades[0]) / len(grades[0])
+print(average)
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+res = list(students)
+print(res)
